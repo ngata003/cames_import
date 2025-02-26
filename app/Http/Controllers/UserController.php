a72640ec41efdc556b615a13f3707e0b68ee5134
ngata003 <ngatamtoullrich@gmail.com>
2025-02-26 21:38:44 +0100

    ajout de la fonction d'insertion des gestionnaires

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Auth;
 use Hash;
 use Illuminate\Http\Request;
+use Session;
+use Str;
 
 class UserController extends Controller
 {
@@ -133,9 +135,63 @@ class UserController extends Controller
     }
 
     public function add_gestionnaires (Request $request){
+
+        $messages = [
+            'name.required' => 'veuillez remplir le champ Nom',
+            'name.regex' => 'Le nom ne doit contenir que des lettres et des espaces.',
+            'name.unique' => 'veuillez changer ce nom car il est déjà enregistré',
+
+            'email.required' => 'veuillez changer votre email car il existe deja',
+            'email.regex' => 'veuillez rentrer une adresse email valide ',
+            'email.unique'=> 'cet email existe deja veuillez utiliser une autre',
+
+            'contact.required' => 'veuillez rentrer un contact',
+            'contact.regex' => 'veuillez rentrer un contact valide',
+            'contact.unique' => 'veuillez rentrer un autre contact car celui-ci existe déjà',
+        ];
+
+
         $request->validate([
-            ''
-        ]);
+            'name' => 'required|string|max:255|unique:users,name|regex:/^[a-zA-ZÀ-ÿ\s-]+$/',
+            'email' => 'required|email|unique:users,email|regex:/^[a-zA-Z]+[a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/',
+            'contact' => 'required|unique:users,contact|regex:/^\+?[1-9]\d{6,14}$/',
+            'type' => 'required',
+            'role' => 'required',
+            'password' => 'required|max:12',
+            'profil' => 'nullable|image|mimes:jpeg,jpg,png,svg,gif|max:2048',
+        ], $messages);
+
+        $passwordFirst = Str::random(10);
+
+        $password = bcrypt($passwordFirst);
+
+        if ($request->hasFile('profil')) {
+            $imageFile = $request->file('profil');
+            $profilName = time().'.'. $imageFile->getClientOriginalExtension();
+            $imageFile->move(public_path('assets/images'),$profilName);
+        }
+
+        $gestionnaires = new User();
+
+        $user = Auth::user();
+        $nom_createur = $user->name;
+
+        $entreprise = Session::get('entreprise_active');
+        $nom_entreprise = $entreprise->nom_entreprise;
+
+        $gestionnaires->name = $request->input('name');
+        $gestionnaires->email = $request->input('email');
+        $gestionnaires->contact = $request->input('contact');
+        $gestionnaires->type = $request->input('type');
+        $gestionnaires->role = $request->input('role');
+        $gestionnaires->profil = $profilName;
+        $gestionnaires->password = $password;
+        $gestionnaires->nom_createur = $nom_createur;
+        $gestionnaires->nom_entreprise = $nom_entreprise;
+
+        $gestionnaires->save();
+
+        return back()->with('save_success','gestionnaire ajouté avec succès');
     }
 
 }
